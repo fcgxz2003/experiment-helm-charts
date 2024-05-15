7165921f481bd6de40c0401cc4d347f5251dff83
XZ <834756128@qq.com>
2024-05-15 09:10:47 +0000

    add shell
    Signed-off-by: XZ <834756128@qq.com>

---
+++ shell/fig11/50pod_500M_default_epoch3.sh
@@ -0,0 +1,36 @@
+#!/bin/bash 
+
+output_file="../fig11_output/50pod_500M_default_epoch3.txt"
+
+tmp_dir=$(mktemp -d)
+
+pod="dragonfly-peer"
+minio_address=$MINIO_ADDRESS
+model="http://$minio_address/models/500M.bin?x=1"
+container="peer"
+namespace="dragonfly-system"
+
+mkdir -p ../fig11_output
+touch ../fig11_output/50pod_500M_default_epoch3.txt
+
+command() {
+  kubectl exec -it $pod-$1 -c peer -n $namespace -- dfget -o /test -u $model  >> "$tmp_dir/$1.txt"
+}
+
+command_delete() {
+  kubectl exec -it $pod-$1 -c peer -n $namespace -- rm -rf /test
+}
+
+for i in $(seq 100 149);do
+  command $i | echo "peer-$i download model"
+done
+
+wait
+
+cat "$tmp_dir"/*.txt > "$output_file"
+
+rm -rf "$tmp_dir"
+
+#for i in $(seq 100 149);do
+#  command_delete $i | echo "peer-$i remove model"
+#done
