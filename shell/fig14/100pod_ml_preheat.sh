8d35701470c131291de4b23285a711b866228082
XZ <834756128@qq.com>
2024-05-22 11:37:06 +0000

    fix
    Signed-off-by: XZ <834756128@qq.com>

--- shell/fig14/100pod_ml_preheat.sh
+++ shell/fig14/100pod_ml_preheat.sh
@@ -2,7 +2,7 @@
 
 pod="dragonfly-peer"
 minio_address=$MINIO_ADDRESS
-model="http://$minio_address/models/10G.bin?x=1003"
+model="http://$minio_address/models/10G.bin?x=1005"
 container="peer"
 namespace="dragonfly-system"
 
