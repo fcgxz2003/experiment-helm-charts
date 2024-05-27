892d510de350f73155d99ff1a90947c11c7a5af2
XZ <834756128@qq.com>
2024-05-27 14:13:28 +0000

    add test preheat
    Signed-off-by: XZ <834756128@qq.com>

---
+++ shell/test/preheat.sh
@@ -0,0 +1,99 @@
+#!/bin/bash 
+
+pod="dragonf-ulg1d8-dragonfly-peer"
+minio_address=$MINIO_ADDRESS
+container="peer"
+namespace="dragonfly-system"
+
+mkdir -p ../fig14_output
+
+# ------------------------------------ 没有预热
+tmp_dir=$(mktemp -d)
+model="http://210.30.96.107:32055/model/5002.bin?x=321"
+
+for i in $(seq 0 1);do
+    kubectl exec -it $pod-$i -c peer -n $namespace -- dfget -o /test -u $model  >> "$$tmp_dir/$i.txt" | echo "peer-$i download model"  &
+done
+
+wait
+
+output_file="../fig14_output/test_preheat_0.txt"
+touch ../fig14_output/test_preheat_0.txt
+cat "$tmp_dir"/*.txt > "$output_file"
+
+rm -rf "$tmp_dir"
+
+for i in $(seq 0 1);do
+    kubectl exec -it $pod-$i -c peer -n $namespace -- rm -rf /test &
+done
+
+wait
+# ------------------------------------ 2个预热的结果
+
+tmp_dir=$(mktemp -d)
+
+for i in $(seq 2 3);do
+    kubectl exec -it $pod-$i -c peer -n $namespace -- dfget -o /test -u $model  >> "$$tmp_dir/$i.txt" | echo "peer-$i download model"  &
+done
+
+wait
+
+output_file="../fig14_output/test_preheat_2.txt"
+touch ../fig14_output/test_preheat_2.txt
+cat "$tmp_dir"/*.txt > "$output_file"
+
+rm -rf "$tmp_dir"
+
+for i in $(seq 2 3);do
+    kubectl exec -it $pod-$i -c peer -n $namespace -- rm -rf /test &
+done
+
+wait
+# ------------------------------------ 6个预热的结果
+
+for i in $(seq 4 5);do
+    kubectl exec -it $pod-$i -c peer -n $namespace -- dfget -o /test -u $model &
+done
+
+wait
+
+tmp_dir=$(mktemp -d)
+
+for i in $(seq 6 7);do
+    kubectl exec -it $pod-$i -c peer -n $namespace -- dfget -o /test -u $model  >> "$tmp_dir/$i.txt" | echo "peer-$i download model"  &
+done
+
+wait
+
+output_file="../fig14_output/test_preheat_6.txt"
+touch ../fig14_output/test_preheat_6.txt
+cat "$tmp_dir"/*.txt > "$output_file"
+
+rm -rf "$tmp_dir"
+
+for i in $(seq 6 7);do
+    kubectl exec -it $pod-$i -c peer -n $namespace -- rm -rf /test &
+done
+
+wait
+# ------------------------------------ 8个预热的结果
+
+tmp_dir=$(mktemp -d)
+
+for i in $(seq 8 9);do
+    kubectl exec -it $pod-$i -c peer -n $namespace -- dfget -o /test -u $model  >> "$$tmp_dir/$i.txt" | echo "peer-$i download model"  &
+done
+
+wait
+
+output_file="../fig14_output/test_preheat_8.txt"
+touch ../fig14_output/test_preheat_9.txt
+cat "$tmp_dir"/*.txt > "$output_file"
+
+rm -rf "$tmp_dir"
+
+for i in $(seq 8 9);do
+    kubectl exec -it $pod-$i -c peer -n $namespace -- rm -rf /test &
+done
+
+wait
